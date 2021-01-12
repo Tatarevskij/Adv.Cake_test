88a8f9f43c8e026c976d97636a0bc6a5f5de20ef
Tatarevskij <37383927+Tatarevskij@users.noreply.github.com>
2021-01-12 22:14:05 +0300

    Create Words.php

---
+++ Words.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class Words
+{
+    public function revertCharacters($string) {
+
+        $result = false;
+        $strToArr = explode(" ", $string);
+        $symbols = array("!", "?", ".");
+
+        foreach ($strToArr as $str){
+            foreach ($symbols as $symbol){
+                $pos = strpos($str, $symbol);
+                if ($pos) {
+                    $str = strstr("$str", "$symbol", true);
+                    break;
+                }
+                $symbol = false;
+            }
+
+            $string = strrev($str) . "$symbol" . " ";
+            if(preg_match('/[А-Я]/', $string)){
+                $string = ucfirst(mb_strtolower($string, 'windows-1251'));
+            }
+            $result = $result . $string;
+
+
+
+        }
+
+        return $result;
+    }
+
+}
